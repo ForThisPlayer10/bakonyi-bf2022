ac2edcecbe509696434c6860b1721cda248716d9
BenceToth <tothbencetamas@gmail.com>
2022-11-03 22:00:57 +0100

    final

---
+++ online/task6/main.js
@@ -0,0 +1,8 @@
+function set_This_Table(index_Of_Table){
+    console.log(index_Of_Table);
+}
+
+function close(){
+    console.log("close")
+    document.getElementById("settings_Window").style.display = "none"; 
+}
